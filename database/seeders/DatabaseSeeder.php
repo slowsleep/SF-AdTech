6b3f5b13cc9366c6c2a3a89c91bd325225895a56
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-11-08 17:58:09 +0300

    Edit DatabaseSeeder: add all seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RoleSeeder;
+use Database\Seeders\SiteThemeSeeder;
+use Illuminate\Support\Facades\Hash;
+use Database\Seeders\OfferSeeder;
+use Database\Seeders\WebmasterOfferSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +22,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call(RoleSeeder::class);
+
+        $this->call(UserSeeder::class);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'password' => Hash::make('admin123'),
+            'email' => 'admin@example.com',
+            'role_id' => Role::where('name', '=', 'admin')->get()->id,
+        ]);
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('test123'),
         ]);
+
+        $this->call(SiteThemeSeeder::class);
+        $this->call(OfferSeeder::class);
+        $this->call(WebmasterOfferSeeder::class);
+
     }
 }
