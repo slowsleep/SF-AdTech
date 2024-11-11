d042304c011e868e41003c72d11f51a6fd5a6ce7
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-11-11 19:50:29 +0300

    Edit admin user index vue view: edit nav, edit display user's data by role

--- resources/js/Pages/Admin/User/Index.vue
+++ resources/js/Pages/Admin/User/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import NavLink from '@/Components/NavLink.vue';
+import Nav from './Partials/Nav.vue';
 </script>
 
 <template>
@@ -9,31 +9,18 @@ import NavLink from '@/Components/NavLink.vue';
 
     <AuthenticatedLayout>
         <template #header>
-            <nav>
-                <NavLink
-                    :href="route('admin.users')"
-                    :active="route().current('admin.users', '')"
-                >
-                    Пользователи
-                </NavLink>
-                <NavLink
-                    :href="route('admin.users', 'advertiser')"
-                    :active="route().current('admin.users', 'advertiser')"
-                >
-                    Рекламодатели
-                </NavLink>
-                <NavLink
-                    :href="route('admin.users', 'webmaster')"
-                    :active="route().current('admin.users', 'webmaster')"
-                >
-                    Веб-мастера
-                </NavLink>
-            </nav>
+            <Nav />
         </template>
 
         <div class="bg-gray-400 p-12">
             <div v-for="user in $page.props.users" :key="user.id">
                 <p>{{ user.name }} - {{ user.email }} - {{ user.role.name }}</p>
+                <p v-if="user.role.name === 'advertiser'">
+                    <i>&emsp;name company: {{ user.advertiser.name ?? '' }}</i>
+                </p>
+                <p v-if="user.role.name === 'webmaster'">
+                    <i>&emsp;site: {{ user.webmaster.site ?? '' }}</i>
+                </p>
             </div>
         </div>
     </AuthenticatedLayout>
