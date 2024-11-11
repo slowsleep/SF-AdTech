d2223ce5aca543fc2dc71b6f3aa8c33861fe4041
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-11-11 19:48:40 +0300

    Add admin user nav vue view

---
+++ resources/js/Pages/Admin/User/Partials/Nav.vue
@@ -0,0 +1,33 @@
+<script setup>
+import NavLink from '@/Components/NavLink.vue';
+
+</script>
+
+<template>
+    <nav>
+        <NavLink
+            :href="route('admin.users.index', '')"
+            :active="route().current('admin.users.index', '')"
+        >
+            Пользователи
+        </NavLink>
+        <NavLink
+            :href="route('admin.users.index', 'advertiser')"
+            :active="route().current('admin.users.index', 'advertiser')"
+        >
+            Рекламодатели
+        </NavLink>
+        <NavLink
+            :href="route('admin.users.index', 'webmaster')"
+            :active="route().current('admin.users.index', 'webmaster')"
+        >
+            Веб-мастера
+        </NavLink>
+        <NavLink
+            :href="route('admin.users.create')"
+            :active="route().current('admin.users.create')"
+        >
+            Создать пользователя
+        </NavLink>
+    </nav>
+</template>
