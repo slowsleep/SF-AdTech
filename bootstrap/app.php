872aca54977bcba0f2d956c967170b7882107775
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-11-22 12:29:36 +0300

    Add alias advertiser to middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'admin' => \App\Http\Middleware\IsAdmin::class,
+            'advertiser' => \App\Http\Middleware\IsAdvertiser::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
