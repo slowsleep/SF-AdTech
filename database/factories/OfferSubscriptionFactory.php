2d5d394875089f48719d921c0eebf6d2307a378f
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-11-26 23:24:31 +0300

    Edit OfferSubscriptionFactory: remove is_subscribed, add unique webmaster_id and offer_id

--- database/factories/OfferSubscriptionFactory.php
+++ database/factories/OfferSubscriptionFactory.php
@@ -18,11 +18,20 @@ class OfferSubscriptionFactory extends Factory
      */
     public function definition(): array
     {
+        static $usedPairs = []; // Хранилище уникальных пар
+
+        do {
+            $webmasterId = Webmaster::all()->random()->user_id;
+            $offerId = Offer::all()->random()->id;
+            $pair = "{$webmasterId}-{$offerId}";
+        } while (in_array($pair, $usedPairs)); // Проверка на уникальность
+
+        $usedPairs[] = $pair;
+
         return [
-            'webmaster_id' => Webmaster::all()->random()->user_id,
-            'offer_id' => Offer::all()->random()->id,
-            'is_subscribed' => fake()->boolean(),
-            'ref_link' => fake()->url(),
+            'webmaster_id' => $webmasterId,
+            'offer_id' => $offerId,
+            'ref_link_uuid' => fake()->uuid(),
         ];
     }
 }
