ca76df477e1ab41995aff071fa9a1afb972e16b1
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-11-29 23:33:59 +0300

    Add offer Subscription component. Edit offer show view

---
+++ resources/js/Pages/Offer/Partials/Subscription.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { usePage } from '@inertiajs/vue3';
+import Statistics from './Statistics.vue';
+
+const hrefOrigin = window.location.origin;
+
+const props = defineProps({
+    offer: {
+        type: Object,
+        required: true,
+    },
+    subscription: {
+        type: Object,
+        required: true,
+    },
+});
+const user = usePage().props.auth.user;
+</script>
+
+<template>
+    <p>id {{ props.subscription.id }}</p>
+    <p>offer_id {{ props.subscription.offer_id }}</p>
+    <p>webmaster_id {{ props.subscription.webmaster_id }}</p>
+    <p>
+        ref_link_uuid
+        <a
+            class="underline"
+            :href="hrefOrigin + '/r/' + props.subscription.ref_link_uuid"
+        >
+            {{ props.subscription.ref_link_uuid }}
+        </a>
+    </p>
+    <p>created_at {{ props.subscription.created_at }}</p>
+    <p>
+        ref_log length
+        {{ props.subscription.ref_log.length }}
+    </p>
+    <div v-if="props.subscription.ref_log.length > 0">
+        <h2>Переходы</h2>
+        <div v-for="log in props.subscription.ref_log" :key="log.id">
+            <p>id: {{ log.id }}</p>
+            <p>created_at: {{ log.created_at }}</p>
+        </div>
+        <Statistics :id="props.subscription.id" />
+        <p v-if="user.role.name == 'advertiser'">
+            Расходы:
+            {{ props.subscription.ref_log.length * props.offer.price }}
+        </p>
+        <p v-else-if="user.role.name == 'webmaster'">
+            Доход:
+            {{ props.subscription.ref_log.length * (0.8 * props.offer.price) }}
+        </p>
+    </div>
+</template>
--- resources/js/Pages/Offer/Show.vue
+++ resources/js/Pages/Offer/Show.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import Statistics from './Partials/Statistics.vue';
+import Subscription from './Partials/Subscription.vue';
 </script>
 
 <template>
@@ -18,6 +18,7 @@ import Statistics from './Partials/Statistics.vue';
             <p>Заказ: {{ $page.props.offer.title }}</p>
             <p>id: {{ $page.props.offer.id }}</p>
             <p>от {{ $page.props.offer.advertiser.name }}</p>
+            <p>ссылка: {{ $page.props.offer.url }}</p>
             <p>стоимость: {{ $page.props.offer.price }}</p>
             <p>тема: {{ $page.props.offer.theme.name }}</p>
             <p>
@@ -31,31 +32,18 @@ import Statistics from './Partials/Statistics.vue';
             v-if="$page.props.auth.user.role.name == 'advertiser'"
         >
             <h2 class="m-4 text-lg">Подписки вебмастеров</h2>
+            <p v-if="!$page.props.offer.subscriptions.length" class="m-4">
+                Подписок нет
+            </p>
             <div
                 class="m-4 border-2 border-dotted p-4"
                 v-for="subscription in $page.props.offer.subscriptions"
                 :key="subscription.id"
             >
-                <p>id {{ subscription.id }}</p>
-                <p>offer_id {{ subscription.offer_id }}</p>
-                <p>webmaster_id {{ subscription.webmaster_id }}</p>
-                <p>ref_link_uuid {{ subscription.ref_link_uuid }}</p>
-                <p>created_at {{ subscription.created_at }}</p>
-                <div v-if="subscription.ref_log.length > 0">
-                    <h2>Переходы</h2>
-                    <div v-for="log in subscription.ref_log" :key="log.id">
-                        <p>id: {{ log.id }}</p>
-                        <p>created_at: {{ log.created_at }}</p>
-                    </div>
-                    <Statistics :id="subscription.id" />
-                    <p>
-                        Расходы:
-                        {{
-                            subscription.ref_log.length *
-                            $page.props.offer.price
-                        }}
-                    </p>
-                </div>
+                <Subscription
+                    :offer="$page.props.offer"
+                    :subscription="subscription"
+                />
             </div>
         </div>
 
@@ -67,39 +55,10 @@ import Statistics from './Partials/Statistics.vue';
             "
         >
             <h2 class="m-4 text-lg">Подписка на заказ</h2>
-            <p>id {{ $page.props.offer.subscription[0].id }}</p>
-            <p>offer_id {{ $page.props.offer.subscription[0].offer_id }}</p>
-            <p>
-                webmaster_id
-                {{ $page.props.offer.subscription[0].webmaster_id }}
-            </p>
-            <p>
-                ref_link_uuid
-                {{ $page.props.offer.subscription[0].ref_link_uuid }}
-            </p>
-            <p>created_at {{ $page.props.offer.subscription[0].created_at }}</p>
-            <p>
-                ref_log length
-                {{ $page.props.offer.subscription[0].ref_log.length }}
-            </p>
-            <div v-if="$page.props.offer.subscription[0].ref_log.length > 0">
-                <h2>Переходы</h2>
-                <div
-                    v-for="log in $page.props.offer.subscription[0].ref_log"
-                    :key="log.id"
-                >
-                    <p>id: {{ log.id }}</p>
-                    <p>created_at: {{ log.created_at }}</p>
-                </div>
-            </div>
-            <Statistics :id="$page.props.offer.subscription[0].id" />
-            <h2 class="text-md m-3">
-                Доход:
-                {{
-                    $page.props.offer.subscription[0].ref_log.length *
-                    (0.8 * $page.props.offer.price)
-                }}
-            </h2>
+            <Subscription
+                :offer="$page.props.offer"
+                :subscription="$page.props.offer.subscription[0]"
+            />
         </div>
     </AuthenticatedLayout>
 </template>
