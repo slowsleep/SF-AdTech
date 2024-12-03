42abac04d959c0f7a941147cb59752974bce1ead
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-12-03 19:19:26 +0300

    Add activate and deactivate user buttons for admin

--- resources/js/Pages/Admin/User/Index.vue
+++ resources/js/Pages/Admin/User/Index.vue
@@ -1,7 +1,40 @@
 <script setup>
-import { Head } from '@inertiajs/vue3';
+import { Head, usePage } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import Nav from './Partials/Nav.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import axios from 'axios';
+
+const users = usePage().props.users;
+
+const userDeactivation = (id) => {
+    axios
+        .patch(route('api.users.update'), { id: id, active: false })
+        .then((response) => {
+            console.log(response);
+            if (!response.data.error) {
+                users.forEach((item) => {
+                    if (item.id == id) {
+                        item.active = false;
+                    }
+                });
+            }
+        });
+};
+const userActivation = (id) => {
+    axios
+        .patch(route('api.users.update'), { id: id, active: true })
+        .then((response) => {
+            console.log(response);
+            if (!response.data.error) {
+                users.forEach((item) => {
+                    if (item.id == id) {
+                        item.active = true;
+                    }
+                });
+            }
+        });
+};
 </script>
 
 <template>
@@ -21,6 +54,18 @@ import Nav from './Partials/Nav.vue';
                 <p v-if="user.role.name === 'webmaster'">
                     <i>&emsp;site: {{ user.webmaster.site ?? '' }}</i>
                 </p>
+                <PrimaryButton
+                    v-if="user.active"
+                    class="m-2"
+                    @click="userDeactivation(user.id)"
+                    >отключить</PrimaryButton
+                >
+                <PrimaryButton
+                    v-else
+                    class="m-2"
+                    @click="userActivation(user.id)"
+                    >включить</PrimaryButton
+                >
             </div>
         </div>
     </AuthenticatedLayout>
